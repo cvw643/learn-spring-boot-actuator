982d8575030dd541ec211774d7cd70424360116f
York Huang <york@ibsettlecn.com>
2020-06-22 23:38:29 +0800

    initial

--- src/main/java/com/github/cvw643/SampleEndpoint.java
+++ src/main/java/com/github/cvw643/SampleEndpoint.java
@@ -2,6 +2,7 @@ package com.github.cvw643;
 
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,5 +11,10 @@ public class SampleEndpoint {
     @ReadOperation
     public String read() {
         return "Hello";
+    }
+
+    @WriteOperation
+    public String echo(String content) {
+        return content;
     }
 }
